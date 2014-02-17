d4caf7b2efd4b45f2315e09ff08419347fc4aa80
Pranav Ravichandran <me@onloop.net>
2014-02-17 23:48:57 +0530

    parseFloat() instead of parseInt(), and increase font-weight transition

--- frequency.js
+++ frequency.js
@@ -6,28 +6,28 @@
 
 $(document).ready(function () {
     $("*[id^=frequency]").each(function () {
-        var frequency = parseInt(($(this).attr('id')).split("-")[1]);
+        var frequency = parseFloat(($(this).attr('id')).split("-")[1]);
         var fadeDelay = Math.log(frequency) / Math.LN10;
         var fadeTimePercentage = fadeDelay * 100 / frequency;
 
         $('head').append("<style>                        \
             @keyframes fade {                            \
-                0%   {opacity:1; font-weight:500;}" + fadeTimePercentage +
+                0%   {opacity:1; font-weight:600;}" + fadeTimePercentage +
                 "%   {opacity:0.25; font-weight:normal;} \
                 100% {opacity:0.25; font-weight:normal;} \
             }                                            \
             @-webkit-keyframes fade {                    \
-                0%   {opacity:1; font-weight:500;}" + fadeTimePercentage +
+                0%   {opacity:1; font-weight:600;}" + fadeTimePercentage +
                 "%   {opacity:0.25; font-weight:normal;} \
                 100% {opacity:0.25; font-weight:normal;} \
             }                                            \
             @-moz-keyframes fade {                       \
-                0%   {opacity:1; font-weight:500;;}" + fadeTimePercentage +
+                0%   {opacity:1; font-weight:600;;}" + fadeTimePercentage +
                 "%   {opacity:0.25; font-weight:normal;} \
                 100% {opacity:0.25; font-weight:normal;} \
             }                                            \
             @-o-keyframes fade {                         \
-                0%   {opacity:1; font-weight:500;}" + fadeTimePercentage +
+                0%   {opacity:1; font-weight:600;}" + fadeTimePercentage +
                 "%   {opacity:0.25; font-weight:normal;} \
                 100% {opacity:0.25; font-weight:normal;} \
             }                                            \
