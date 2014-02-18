0291806d779eb980a142de4db897ecaddc3a7bc0
Pranav Ravichandran <me@onloop.net>
2014-02-18 16:44:29 +0530

    More prolonged fade for very high frequency

--- frequency.js
+++ frequency.js
@@ -10,31 +10,31 @@ $(document).ready(function () {
     $("*[id^=frequency]").each(function () {
         var frequency = parseFloat(($(this).attr('id')).split("-")[1]);
         var fadeDelay = Math.log(frequency) / Math.LN10;
-        var fadeTimePercentage = fadeDelay * 100 / frequency;
+        var fadeTimePercentage = (frequency >= 1 && frequency < 2) ? 30 : fadeDelay * 100 / frequency;
         var aName = 'fade' + count;
         var initialFontWeight = $(this).css('font-weight');
-        var finalFontWeight = initialFontWeight <= 600 ? initialFontWeight * 1.5 : initialFontWeight;
+        var finalFontWeight = (initialFontWeight <= 600) ? initialFontWeight * 1.5 : initialFontWeight;
 
         $('head').append("<style>                                           \
             @keyframes " + aName + " {                                      \
                 0%   {opacity:1; font-weight:" + finalFontWeight + ";}" + fadeTimePercentage +
                 "%   {opacity:0.25; font-weight:" + initialFontWeight + ";} \
-                100% {opacity:0.25; font-weight:" + initialFontWeight + ";} \
+                100% {opacity:0.25;}                                        \
             }                                                               \
             @-webkit-keyframes " + aName + " {                              \
                 0%   {opacity:1; font-weight:" + finalFontWeight + ";}" + fadeTimePercentage +
                 "%   {opacity:0.25; font-weight:" + initialFontWeight + ";} \
-                100% {opacity:0.25; font-weight:" + initialFontWeight + ";} \
+                100% {opacity:0.25;}                                        \
             }                                                               \
             @-moz-keyframes " + aName + " {                                 \
                 0%   {opacity:1; font-weight:" + finalFontWeight + ";}" + fadeTimePercentage +
                 "%   {opacity:0.25; font-weight:" + initialFontWeight + ";} \
-                100% {opacity:0.25; font-weight:" + initialFontWeight + ";} \
+                100% {opacity:0.25;}                                        \
             }                                                               \
             @-o-keyframes " + aName + " {                                   \
                 0%   {opacity:1; font-weight:" + finalFontWeight + ";}" + fadeTimePercentage +
                 "%   {opacity:0.25; font-weight:" + initialFontWeight + ";} \
-                100% {opacity:0.25; font-weight:" + initialFontWeight + ";} \
+                100% {opacity:0.25;}                                        \
             }                                                               \
         </style>");
 
