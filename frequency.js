4e9919ddb712c7834a22f20fb635e83c494360be
Pranav Ravichandran <me@onloop.net>
2014-02-18 10:10:28 +0530

    Use individual keyframes for each fade animation type

--- frequency.js
+++ frequency.js
@@ -5,43 +5,50 @@
  */
 
 $(document).ready(function () {
+    var count = 0;
+
     $("*[id^=frequency]").each(function () {
         var frequency = parseFloat(($(this).attr('id')).split("-")[1]);
         var fadeDelay = Math.log(frequency) / Math.LN10;
         var fadeTimePercentage = fadeDelay * 100 / frequency;
+        var aName = 'fade' + count;
+        var initialFontWeight = $(this).css('font-weight');
+        var finalFontWeight = initialFontWeight <= 600 ? initialFontWeight * 1.5 : initialFontWeight;
 
-        $('head').append("<style>                        \
-            @keyframes fade {                            \
-                0%   {opacity:1; font-weight:600;}" + fadeTimePercentage +
-                "%   {opacity:0.25; font-weight:normal;} \
-                100% {opacity:0.25; font-weight:normal;} \
-            }                                            \
-            @-webkit-keyframes fade {                    \
-                0%   {opacity:1; font-weight:600;}" + fadeTimePercentage +
-                "%   {opacity:0.25; font-weight:normal;} \
-                100% {opacity:0.25; font-weight:normal;} \
-            }                                            \
-            @-moz-keyframes fade {                       \
-                0%   {opacity:1; font-weight:600;;}" + fadeTimePercentage +
-                "%   {opacity:0.25; font-weight:normal;} \
-                100% {opacity:0.25; font-weight:normal;} \
-            }                                            \
-            @-o-keyframes fade {                         \
-                0%   {opacity:1; font-weight:600;}" + fadeTimePercentage +
-                "%   {opacity:0.25; font-weight:normal;} \
-                100% {opacity:0.25; font-weight:normal;} \
-            }                                            \
+        $('head').append("<style>                                 \
+            @keyframes " + aName + " {                            \
+                0%   {opacity:1; font-weight:" + finalFontWeight + ";}" + fadeTimePercentage +
+                "%   {opacity:0.25; font-weight:" + initialFontWeight + ";} \
+                100% {opacity:0.25; font-weight:" + initialFontWeight + ";} \
+            }                                                     \
+            @-webkit-keyframes " + aName + " {                    \
+                0%   {opacity:1; font-weight:" + finalFontWeight + ";}" + fadeTimePercentage +
+                "%   {opacity:0.25; font-weight:" + initialFontWeight + ";} \
+                100% {opacity:0.25; font-weight:" + initialFontWeight + ";} \
+            }                                                     \
+            @-moz-keyframes " + aName + " {                       \
+                0%   {opacity:1; font-weight:" + finalFontWeight + ";}" + fadeTimePercentage +
+                "%   {opacity:0.25; font-weight:" + initialFontWeight + ";} \
+                100% {opacity:0.25; font-weight:" + initialFontWeight + ";} \
+            }                                                     \
+            @-o-keyframes " + aName + " {                         \
+                0%   {opacity:1; font-weight:" + finalFontWeight + ";}" + fadeTimePercentage +
+                "%   {opacity:0.25; font-weight:" + initialFontWeight + ";} \
+                100% {opacity:0.25; font-weight:" + initialFontWeight + ";} \
+            }                                                     \
         </style>");
 
         $(this).css({
-            "animation": "fade " + frequency + "s forwards",
-            "-webkit-animation": "fade " + frequency + "s forwards",
-            "-moz-animation": "fade " + frequency + "s forwards",
-            "-o-animation": "fade " + frequency + "s forwards",
+            "animation": aName + " " + frequency + "s forwards",
+            "-webkit-animation": aName + " " + frequency + "s forwards",
+            "-moz-animation": aName + " " + frequency + "s forwards",
+            "-o-animation": aName + " " + frequency + "s forwards",
             "animation-iteration-count": "infinite",
             "-webkit-animation-iteration-count": "infinite",
             "-moz-animation-iteration-count": "infinite",
             "-o-animation-iteration-count": "infinite"
         });
+
+        count++;
     });
 });
