7d98717753dc08a03c36227ea88f691be30c6c10
Sam Stonehouse <sam@echographics.co.uk>
2014-02-19 16:08:57 +0000

    Changed to plugin & added timeUnit option

--- frequency.js
+++ frequency.js
@@ -4,49 +4,70 @@
  * http://github.com/pranavrc/frequency/
  */
 
-$(document).ready(function () {
+(function($){
     var count = 0;
 
-    $("[data-frequency]").each(function () {
-        var frequency = parseFloat(($(this).data('frequency')));
-        var fadeDelay = Math.log(frequency) / Math.LN10;
-        var fadeTimePercentage = (frequency <= 2) ? 75 : fadeDelay * 100 / frequency;
-        var aName = 'fade' + count;
+    $.frequency = function(el, options) {
+        var base = this;
 
-        $('head').append("<style>                         \
-            @keyframes " + aName + " {                    \
-                0%   {opacity:1;}" + fadeTimePercentage +
-                "%   {opacity:0.10;}                      \
-                100% {opacity:0.10;}                      \
-            }                                             \
-            @-webkit-keyframes " + aName + " {            \
-                0%   {opacity:1;}" + fadeTimePercentage +
-                "%   {opacity:0.10;}                      \
-                100% {opacity:0.10;}                      \
-            }                                             \
-            @-moz-keyframes " + aName + " {               \
-                0%   {opacity:1;}" + fadeTimePercentage +
-                "%   {opacity:0.10;}                      \
-                100% {opacity:0.10;}                      \
-            }                                             \
-            @-o-keyframes " + aName + " {                 \
-                0%   {opacity:1;}" + fadeTimePercentage +
-                "%   {opacity:0.10;}                      \
-                100% {opacity:0.10;}                      \
-            }                                             \
-        </style>");
+        base.el = el;
+        base.$el = $(el);
 
-        $(this).css({
-            "animation": aName + " " + frequency + "s forwards",
-            "-webkit-animation": aName + " " + frequency + "s forwards",
-            "-moz-animation": aName + " " + frequency + "s forwards",
-            "-o-animation": aName + " " + frequency + "s forwards",
-            "animation-iteration-count": "infinite",
-            "-webkit-animation-iteration-count": "infinite",
-            "-moz-animation-iteration-count": "infinite",
-            "-o-animation-iteration-count": "infinite"
+        base.options = $.extend({},$.frequency.defaultOptions, options);
+
+        base.init = function(){
+            var frequency = parseFloat(base.$el.data('frequency') * base.options.timeUnit);
+            var fadeDelay = Math.log(frequency) / Math.LN10;
+            var fadeTimePercentage = (frequency <= 2) ? 75 : fadeDelay * 100 / frequency;
+            var aName = 'fade' + count;
+
+            $('head').append("<style>                         \
+                @keyframes " + aName + " {                    \
+                    0%   {opacity:1;}" + fadeTimePercentage +
+                    "%   {opacity:0.10;}                      \
+                    100% {opacity:0.10;}                      \
+                }                                             \
+                @-webkit-keyframes " + aName + " {            \
+                    0%   {opacity:1;}" + fadeTimePercentage +
+                    "%   {opacity:0.10;}                      \
+                    100% {opacity:0.10;}                      \
+                }                                             \
+                @-moz-keyframes " + aName + " {               \
+                    0%   {opacity:1;}" + fadeTimePercentage +
+                    "%   {opacity:0.10;}                      \
+                    100% {opacity:0.10;}                      \
+                }                                             \
+                @-o-keyframes " + aName + " {                 \
+                    0%   {opacity:1;}" + fadeTimePercentage +
+                    "%   {opacity:0.10;}                      \
+                    100% {opacity:0.10;}                      \
+                }                                             \
+            </style>");
+
+            base.$el.css({
+                "animation": aName + " " + frequency + "s forwards",
+                "-webkit-animation": aName + " " + frequency + "s forwards",
+                "-moz-animation": aName + " " + frequency + "s forwards",
+                "-o-animation": aName + " " + frequency + "s forwards",
+                "animation-iteration-count": "infinite",
+                "-webkit-animation-iteration-count": "infinite",
+                "-moz-animation-iteration-count": "infinite",
+                "-o-animation-iteration-count": "infinite"
+            });
+
+            count++;
+        };
+
+        base.init();
+    }
+
+    $.frequency.defaultOptions = {
+        timeUnit: 1,
+    };
+    
+    $.fn.frequency = function(options){
+        return this.each(function(){
+            (new $.frequency(this, options));
         });
-
-        count++;
-    });
-});
+    };
+})(jQuery);
